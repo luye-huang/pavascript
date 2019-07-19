8deb8a851d8aaae1a14090e548dcdb685a75e153
huangluye <huangluye@qiyi.com>
2019-07-19 13:57:35 +0800

    a entry version of array functions

--- src/array.js
+++ src/array.js
@@ -1,9 +1,19 @@
+import { isEqual } from './utils';
+
 Array.prototype.append = [].push;
+
+Array.prototype.clear = function () {
+    this = [];
+}
+
+Array.prototype.clear = function () {
+    return [...this];
+}
 
 Array.prototype.count = function (obj) {
     let ret = 0, index = 0;
     while (index < this.length) {
-        if (JSON.stringify(obj) == JSON.stringify(this[index])) {
+        if (isEqual(this[index], obj)) {
             ret++;
         }
         index++;
@@ -11,7 +21,20 @@ Array.prototype.count = function (obj) {
     return ret;
 }
 
+Array.prototype.extend = [].push;
+
 Array.prototype.index = [].indexOf;
+
+Array.prototype.insert = function (index, obj) {
+    this.splice(index, 0, obj);
+}
+
+Array.prototype.remove = function (obj) {
+    const idx = this.findIndex(el => isEqual(el, obj));
+    this.splice(idx, 1);
+}
+
+//to do: sort
 
 window.len = function (arr) {
     if ((arr).constructor !== Array) {
